94aae7252398e8be592f328afb8fb3720b02264a
CodeWithSk <mh18gamming@gmail.com>
2021-06-08 13:59:50 +0530

    new commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,53 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  debugShowCheckedModeBanner: false,
       title: "Sagar koli",
-      home: homeScreen(),
+      home: HomeScreen(),
+      theme: ThemeData(
+        primarySwatch: Colors.purple
+      ),
     ));
 
-class homeScreen extends StatelessWidget {
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var appBar2 = AppBar(
-        title: Center(child: Text("The Smart Salon Manger")),
-        backgroundColor: Colors.black,
-      );
+
     return Scaffold(
-      appBar: appBar2,
-      body: Padding(padding: const EdgeInsets.all(8.0),
-      child: Align(
-                alignment: Alignment.bottomRight,
-                    child: Container(
-                      color: Colors.black,
-                      width: 200,
-                      height: 400,
-                      child : Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                      Container(
-                        padding: const EdgeInsets.all(8),
-                        width: 100,
-                        height: 100,
-                        color: Colors.red,
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(8),
-                        width: 100,
-                        height: 100,
-                        color: Colors.yellow,
-                      ),
-                      Container(
-                        padding: const EdgeInsets.all(8),
-                        width: 100,
-                        height: 100,
-                        color: Colors.green,
-                      ),
-                    ],
-                  ),
-                  ),
-              ),       
-      )
+      
+      body: Container(
+
+      ),
+      appBar: AppBar(
+        title: new Text("sagar koli"),
+      ),
+      drawer: Drawer(
+        child: ListView(
+            children: [
+              DrawerHeader(child: Text("I am drawer",
+                            style: TextStyle(color: Colors.white),
+                            ),
+              decoration: BoxDecoration(color: Colors.teal)
+              ),
+              ListTile(
+                  leading: Icon(Icons.person),
+                  title: Text("Account"),
+                  subtitle: Text("This is full details"),
+                  trailing: Icon(Icons.edit),
+              ),
+              ListTile(
+                  leading: Icon(Icons.feedback),
+                  title: Text("feedback"),
+                  subtitle: Text("please give your feed back to improve more functionlity"),
+                  trailing: Icon(Icons.refresh)
+              )
+            ],
+        )
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        
+      },
+      child: Icon(Icons.edit)
+      ),
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,13 @@ import 'package:example/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(SagarApp());
+    await tester.pumpWidget(MaterialApp(
+      title: "Sagar koli",
+      home: HomeScreen(),
+      theme: ThemeData(
+        primarySwatch: Colors.purple
+      ),
+    ));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
