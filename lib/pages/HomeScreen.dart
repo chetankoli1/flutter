2cfe9129f3f0e58776142f7675ffe4e476e83f2a
CodeWithSk <mh18gamming@gmail.com>
2021-06-09 15:18:42 +0530

    JsonParsing

--- lib/pages/HomeScreen.dart
+++ lib/pages/HomeScreen.dart
@@ -1,6 +1,7 @@
-import 'package:example/changeNameCard.dart';
 import 'package:example/drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -10,10 +11,21 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   TextEditingController _nameController = TextEditingController();
   var myText = "Change me";
+  var urls = "https://jsonplaceholder.typicode.com/photos";
+  var data;
 
   @override
   void initState() {
     super.initState();
+
+    getData();
+  }
+
+  getData() async {
+    var res = await http.get(Uri.parse(urls));
+    data = jsonDecode(res.body);
+    print(data);
+    setState(() {});
   }
 
   @override
@@ -24,12 +36,24 @@ class _HomeScreenState extends State<HomeScreen> {
         body: Center(
             child: Padding(
           padding: EdgeInsets.all(16.0),
-          child: SingleChildScrollView(
-            child: Card(
-              child: ChangeNameCard(
-                  myText: myText, nameController: _nameController),
-            ),
-          ),
+          child: data != null
+              ? Card(
+                  child: ListView.builder(
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: ListTile(
+                            title: Text(data[index]["title"]),
+                            leading: Image.network(data[index]["url"]),
+                            subtitle: Text("ID: ${data[index]["id"]}"),
+                          ),
+                        );
+                      },
+                      itemCount: data.length),
+                )
+              : Center(
+                  child: CircularProgressIndicator(),
+                ),
         )),
         drawer: MyDrawer(),
         floatingActionButton: FloatingActionButton(
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,10 +5,9 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:example/pages/HomeScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:example/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
@@ -16,9 +15,7 @@ void main() {
     await tester.pumpWidget(MaterialApp(
       title: "Sagar koli",
       home: HomeScreen(),
-      theme: ThemeData(
-        primarySwatch: Colors.purple
-      ),
+      theme: ThemeData(primarySwatch: Colors.purple),
     ));
 
     // Verify that our counter starts at 0.
